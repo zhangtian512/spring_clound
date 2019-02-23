7cac770a48a3206f10d267d6c2d795da3eb05f27
zt <287839096@qq.com>
2019-02-23 19:34:57 +0800

    config center

--- controller/src/main/java/com/zt/ms/controller/mainController.java
+++ controller/src/main/java/com/zt/ms/controller/mainController.java
@@ -1,11 +1,13 @@
 package com.zt.ms.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class mainController {
 
     @Value("${spring.application.name}")
@@ -17,5 +19,10 @@ public class mainController {
     @RequestMapping(value="main")
     public String main(){
         return name+"+"+port;
+    }
+
+    @RequestMapping(value="getname")
+    public String getname(){
+        return name;
     }
 }
