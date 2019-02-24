c3aa46d8d3ac896b40fcc1d1e91ff838175073f0
zt <287839096@qq.com>
2019-02-24 22:49:56 +0800

    docker 调试

--- config-server/src/main/java/com/zt/ms/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/zt/ms/configserver/ConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.zt.ms.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -10,8 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableConfigServer
 public class ConfigServerApplication {
 
+    private static Logger log = LoggerFactory.getLogger(ConfigServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+        log.info("++++++++++ConfigServerApplication+++++++++++++");
     }
 
 }
--- eureka-server/src/main/java/com/zt/ms/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/zt/ms/eurekaserver/EurekaServerApplication.java
@@ -1,5 +1,7 @@
 package com.zt.ms.eurekaserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,8 +10,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
+    private static Logger log = LoggerFactory.getLogger(EurekaServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        log.info("+++++++++++++EurekaServerApplication+++++++++++++++");
     }
 
 }
